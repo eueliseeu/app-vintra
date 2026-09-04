9022d9bcae81724b06be499a77d904764cf99266
Eliseu Silva <eueliseeu@gmail.com>
2026-09-04 18:10:49 -0300

    feat: remover as classes ConnectionStatus e ConnectionTestViewModel

--- app/src/main/java/com/vintra/app/core/connection/ConnectionStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.vintra.app.core.connection
-
-sealed interface ConnectionStatus {
-    data object Loading : ConnectionStatus
-    data class Success(val message: String, val timestamp: Long) : ConnectionStatus
-    data class Error(val message: String) : ConnectionStatus
-}
