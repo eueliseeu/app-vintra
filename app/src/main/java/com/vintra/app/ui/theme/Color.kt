213b41fa7aaabe1770dcdf5373ce14661a10d886
Eliseu Silva <eueliseeu@gmail.com>
2026-09-04 05:06:17 -0300

    feat: atualizar a cor DarkBackground para preto e adicionar novo ícone do aplicativo

--- app/src/main/java/com/vintra/app/ui/theme/Color.kt
+++ app/src/main/java/com/vintra/app/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.vintra.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkBackground = Color(0xFF131313)
+val DarkBackground = Color(0xFF000000)
 val WhiteText = Color(0xFFFFFFFF)
 val ButtonSave = Color(0xFFB8A517)
 val ButtonDanger = Color(0xFFCD4C4C)
