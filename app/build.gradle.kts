94af564a93a83eaaf60834194382a9399fb30a99
Eliseu Silva <eueliseeu@gmail.com>
2026-09-10 22:09:39 -0300

    feat: Adicionar dependências para as bibliotecas AndroidX Credentials e Google ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,5 +72,8 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.8")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.google.id)
 }
 
